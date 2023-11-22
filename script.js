bb3ddfddb6fa54217525f43f47816d5a96fc0111
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 09:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var isDate = function (input) {
   //   write your code here
+	 var testDate = new Date(input);
+
+    // Check if the input is a valid date and the parsed date is not invalid
+    return !isNaN(testDate.getTime());
 };
+
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
